45dc18a8801c0f7de0230e24d5dbe33853348d02
realm10890 <59128537+realm10890@users.noreply.github.com>
2020-02-02 01:47:21 -0800

    Initial Commit

---
+++ GitRepoTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitRepoTest
+//
+//  Created by Cesar Almendarez on 2/2/20.
+//  Copyright © 2020 Cesar Almendarez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
